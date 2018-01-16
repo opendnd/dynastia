88e957969b822e4bc06480761f47335822ad71da
Drew Morris <dhmorris@gmail.com>
2018-01-15 22:29:54 -0500

    Adding base functionality

--- test/lib/dynastia.test.js
+++ test/lib/dynastia.test.js
@@ -3,17 +3,26 @@ const path = require('path');
 const rootDir = path.join(__dirname, '..', '..');
 const libDir = path.join(rootDir, 'lib');
 const Dynastia = require(path.join(libDir, 'dynastia'));
+const Renderer = require(path.join(libDir, 'renderer'));
 let dynastia, dynasty;
 
 describe('Dynastia', () => {
   before(() => {
-    dynastia = new Dynastia();
+    dynastia = new Dynastia({
+      startingYear: 1066,
+      race: 'Human',
+      theme: 'medieval',
+      generationsCount: 3,
+      inheritance: 'patrilineality',
+    });
   });
 
   it('can generate', () => {
     dynasty = dynastia.generate();
 
     expect(dynasty).to.be.an('object');
+    expect(dynasty.generations.regnant).to.be.a('string');
+    expect(dynasty.generations.consort).to.be.a('string');
   });
 
   it('getDefaults returns defaults with genders and races', () => {
